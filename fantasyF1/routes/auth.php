e773ba568c5354c437f169676a23f0afd7d0ea8c
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2024-03-29 16:13:51 +0100

    pequeño cambio arreglado a auth

--- fantasyF1/routes/auth.php
+++ fantasyF1/routes/auth.php
@@ -1,14 +1,11 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('guest')->group(function () {
-
     Route::post('/login', [RegisterController::class, 'register']);
-    Route::post('/login', [LoginController::class, 'login'])->name('login');
 
    /* Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
@@ -30,7 +27,6 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::post('/login', [RegisterController::class, 'register']);
-    Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 
  /*   Route::get('verify-email', EmailVerificationPromptController::class)
