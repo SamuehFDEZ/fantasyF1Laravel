b10f7b3cd4ecc501e5fc8d2eefd4a3453c140b8d
SamuehFDEZ <samu.ar.lo.04@gmail.com>
2024-04-02 17:32:04 +0200

    pagina de constructores casi hecha, falta confrontacion con footer, opcional la separacion de las columnas

---
+++ fantasyF1/resources/views/partials/coches.blade.php
@@ -0,0 +1,238 @@
+<div class="row m-3">
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">1</span>
+            <span id="puntos">97 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Red Bull</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/redBull.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/redBull.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Max <b>Verstappen</b></div>
+            <div class="piloto">Sergio <b>Pérez</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/redBull.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/redBull.avif")}}">
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">2</span>
+            <span id="puntos">93 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Ferrari</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/ferrari.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/ferrari.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Charles <b>Leclerc</b></div>
+            <div class="piloto">Carlos <b>Sainz</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/ferrari.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/ferrari.avif")}}">
+        </div>
+    </div>
+</div>
+
+<div class="row m-3">
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">3</span>
+            <span id="puntos">55 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Mclaren</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/mclaren.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/mclaren.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Lando <b>Norris</b></div>
+            <div class="piloto">Oscar <b>Piastri</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/mclaren.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/mclaren.avif")}}">
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">4</span>
+            <span id="puntos">26 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Mercedes</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/mercedes.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/mercedes.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Lewis <b>Hamilton</b></div>
+            <div class="piloto">George <b>Russell</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/mercedes.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/mercedes.avif")}}">
+        </div>
+    </div>
+</div>
+
+<div class="row m-3">
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">5</span>
+            <span id="puntos">25 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Aston Martin</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/astonMartin.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/astonMartin.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Fernando <b>Alonso</b></div>
+            <div class="piloto">Lance <b>Stroll</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/astonMartin.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/astonMartin.avif")}}">
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">6</span>
+            <span id="puntos">6 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Visa RB</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/visaRB.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/visaRB.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Daniel <b>Ricciardo</b></div>
+            <div class="piloto">Yuki <b>Tsunoda</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/visaRB.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/visaRB.avif")}}">
+        </div>
+    </div>
+</div>
+
+<div class="row m-3">
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">7</span>
+            <span id="puntos">4 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Hass</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/hass.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/hass.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Nico <b>Hulkenberg</b></div>
+            <div class="piloto">Kevin <b>Magnussen</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/hass.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/hass.avif")}}">
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">8</span>
+            <span id="puntos">0 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Williams</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/williams.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/williams.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Alex <b>Albon</b></div>
+            <div class="piloto">Logan <b>Sargeant</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/williams.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/williams.avif")}}">
+        </div>
+    </div>
+</div>
+
+<div class="row m-3">
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">9</span>
+            <span id="puntos">0 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Sauber</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/stake.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/stake.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Zhou <b>Guanyu</b></div>
+            <div class="piloto">Valteri <b>Bottas</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/stake.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/stake.avif")}}">
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div id="numYPuntos">
+            <span id="num">10</span>
+            <span id="puntos">0 <span>PTS</span></span>
+        </div>
+        <div id="infoEquipo">
+            <div id="barra"></div>
+            <div id="nombre">Alpine</div>
+            <div id="logo">
+                <img src="{{asset("img/teamLogos/alpine.avif")}}" alt="logo"
+                     srcset="{{asset("img/teamLogos/alpine.avif")}}">
+            </div>
+        </div>
+        <div id="pilotosEnEq">
+            <div class="piloto">Pierre <b>Gasly</b></div>
+            <div class="piloto">Esteban <b>Ocon</b></div>
+        </div>
+        <div id="imgCoche">
+            <img src="{{asset("img/coches/alpine.avif")}}" alt="coche"
+                 srcset="{{asset("img/coches/alpine.avif")}}">
+        </div>
+    </div>
+</div>
